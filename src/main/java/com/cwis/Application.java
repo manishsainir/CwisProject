f996251fd0aab36d4ad2921131678727eb2f751c
manishsainir <sainimanish700@gmail.com>
2020-07-15 13:10:26 +0530

    heruko dependency

--- src/main/java/com/cwis/Application.java
+++ src/main/java/com/cwis/Application.java
@@ -7,14 +7,18 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
 import org.springframework.boot.autoconfigure.jms.JndiConnectionFactoryAutoConfiguration;
 import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
-import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = { HibernateJpaAutoConfiguration.class, JndiConnectionFactoryAutoConfiguration.class,
 		DataSourceAutoConfiguration.class, JpaRepositoriesAutoConfiguration.class,
-		DataSourceTransactionManagerAutoConfiguration.class, ErrorMvcAutoConfiguration.class })
+		DataSourceTransactionManagerAutoConfiguration.class})
 public class Application extends SpringBootServletInitializer {
+	
+	public Application() {
+		super();
+		setRegisterErrorPageFilter(false);
+	}
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
@@ -26,14 +30,4 @@ public class Application extends SpringBootServletInitializer {
 
 	}
 
-	/*
-	 * @Bean public ErrorPageFilter errorPageFilter() { return new
-	 * ErrorPageFilter(); }
-	 * 
-	 * @Bean public FilterRegistrationBean
-	 * disableSpringBootErrorFilter(ErrorPageFilter filter) { FilterRegistrationBean
-	 * filterRegistrationBean = new FilterRegistrationBean();
-	 * filterRegistrationBean.setFilter(filter);
-	 * filterRegistrationBean.setEnabled(false); return filterRegistrationBean; }
-	 */
 }
